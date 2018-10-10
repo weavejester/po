1636d58fbe7e2132f2236c13c6f982f4deccc62e
James Reeves <jreeves@weavejester.com>
2018-10-10 23:51:52 +0100

    Add basic command building from yaml

--- po.go
+++ po.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 )
 
 var rootCmd = &cobra.Command{
@@ -14,27 +15,53 @@ var rootCmd = &cobra.Command{
 	Short:   "FIXME",
 	Long:    "FIXME",
 	Version: "0.0.1",
-	Run:     func(c *cobra.Command, args []string) {
-		dat, err := ioutil.ReadFile("po.yml")
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		m := make(map[interface{}]interface{})
-
-		err = yaml.Unmarshal(dat, &m)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println(m)
+	Run: func(c *cobra.Command, args []string) {
 	},
+}
+
+func readCommandsFromYamlFile(path string, m *map[string]string) error {
+	dat, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		return err
+	}
+
+	err = yaml.Unmarshal(dat, &m)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func buildCommands(parentCmd *cobra.Command, m *map[string]string) {
+	for k, v := range *m {
+		cmd := &cobra.Command{
+			Use: k,
+			Run: func(c *cobra.Command, args []string) {
+				shellCmd := exec.Command("sh", "-c", v)
+				shellCmd.Stdin = os.Stdin
+				shellCmd.Stdout = os.Stdout
+				shellCmd.Stderr = os.Stderr
+				shellCmd.Run()
+			},
+		}
+
+		parentCmd.AddCommand(cmd)
+	}
 }
 
 func init() {
 	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+
+	m := make(map[string]string)
+
+	if err := readCommandsFromYamlFile("po.yml", &m); err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	buildCommands(rootCmd, &m)
 }
 
 func main() {
